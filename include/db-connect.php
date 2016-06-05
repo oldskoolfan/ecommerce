7747a75b65da03e37f2f0674a2aa756905d5ec4c
Andrew Harris <harris.1305@gmail.com>
2016-06-04 21:20:15 -0400

    updated db config

--- include/db-connect.php
+++ include/db-connect.php
@@ -1,7 +1,4 @@
 <?php
-	//echo phpinfo();
-	//$con = new mysqli('localhost','root','','beerstoredb');
-	$home = getenv('HOME');
-	include "$home/etc/db-connect.php";
+	include "/etc/db-connect.php";
 	$con->select_db('beerstoredb');
 ?>
