b3476d87897ba05d9dfd68b5fa7451ff5debd693
Andrew Harris <harris.1305@gmail.com>
2016-11-06 11:58:07 -0500

    database fix

--- include/db-connect.php
+++ include/db-connect.php
@@ -1,4 +1,6 @@
 <?php
-	include "/etc/db-connect.php";
-	$con->select_db('beerstoredb');
-?>
+$config = parse_ini_file("{$_SERVER['DOCUMENT_ROOT']}/config.ini");
+$etcPath = $config['etc_directory'];
+include "$etcPath/db-connect.php";
+
+$con->select_db('beerstoredb');
